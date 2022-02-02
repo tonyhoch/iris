b85177a1b09e0365367c510fd2d9fb1be1da03da
tonyhoch <47786838+tonyhoch@users.noreply.github.com>
2022-02-02 17:49:27 -0500

    Update iris-app.py

--- iris-app.py
+++ iris-app.py
@@ -116,7 +116,7 @@ st.write(final_pred_df)
 
 st.write("Prediction Probability")
 pred_prob = loaded_model.predict_proba(scaled_sample)
-pred_prob_df = pd.Dataframe(pred_prob)
+pred_prob_df = pd.DataFrame(pred_prob)
 pred_prob_df = pred_prob_df.rename(columns={0:"Setosa", 1:"Versicolor", 2:"Virginica"})
 
 st.write(pred_prob_df)
